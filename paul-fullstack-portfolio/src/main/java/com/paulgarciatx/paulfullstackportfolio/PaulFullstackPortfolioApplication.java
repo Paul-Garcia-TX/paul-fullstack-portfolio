4ef123f63a02a09826d5b083ec2486dd7ff3bffb
Paul Garcia <122048053+Paul-Garcia-TX@users.noreply.github.com>
2023-07-09 20:00:01 -0500

    Add files via upload

---
+++ paul-fullstack-portfolio/src/main/java/com/paulgarciatx/paulfullstackportfolio/PaulFullstackPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.paulgarciatx.paulfullstackportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaulFullstackPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaulFullstackPortfolioApplication.class, args);
+	}
+
+}
---
+++ paul-fullstack-portfolio/src/test/java/com/paulgarciatx/paulfullstackportfolio/PaulFullstackPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulgarciatx.paulfullstackportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulFullstackPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
